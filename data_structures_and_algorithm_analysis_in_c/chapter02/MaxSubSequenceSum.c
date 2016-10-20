d81585354f9f3612a2862e6a40e062e52590a8bd
youthy <ashura@yuyouqideMBP.lan>
2016-10-21 02:12:18 +0800

    分治法求最大子序列和

--- data_structures_and_algorithm_analysis_in_c/chapter02/MaxSubSequenceSum.c
+++ data_structures_and_algorithm_analysis_in_c/chapter02/MaxSubSequenceSum.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+// 暴力求解
 // O(n^3)
 int
 max_subsequence_sum(const int a[], int n){
@@ -17,6 +20,7 @@ max_subsequence_sum(const int a[], int n){
   return max;
 }
 
+//O(n^2)
 int
 max_subsequence_sum2(const int a[], int n){
   int max=0;
@@ -31,6 +35,7 @@ max_subsequence_sum2(const int a[], int n){
   return max;
 }
 
+//动态规划
 // O(n)
 int
 max_subsequence_sum4(const int a[], int n){
@@ -46,12 +51,42 @@ max_subsequence_sum4(const int a[], int n){
   return max;
 }
 
+//分治
+// O(NlgN)
+int
+max_subsequence_sum3(const int a[], int start, int end){
+  if(start == end){
+    return a[start];
+  }
+  int mid = (end - start )/2 + start;
+  int left, right;
+  left = max_subsequence_sum3(a, start, mid);
+  right = max_subsequence_sum3(a, mid + 1, end);
+  int left2,right2,tmp;
+  left2 = right2 = INT_MIN;
+  tmp = 0;
+  for(int i = mid; i >= start; i--){
+    tmp += a[i];
+    if(tmp > left2)
+    left2 = tmp;
+  };
+  tmp = 0;
+  for(int i = mid + 1; i <= end; i++){
+    tmp += a[i];
+    if(tmp > right2)
+    right2 = tmp;
+  };
+  int plus = left2 + right2;
+  return left>right?(left>plus?left:plus):(right>plus?right:plus);
+}
+
 int
 main(int argc, char const *argv[]) {
   /* code */
-  int a[] ={1,-3,5,19,-5,10};
-  printf("%i\n", max_subsequence_sum(a, 6));
-  printf("%i\n", max_subsequence_sum2(a, 6));
-  printf("%i\n", max_subsequence_sum4(a, 6));
+  int a[] ={1,-3,5,19,-5,10, 23,-14,9,1,20};
+  printf("%i\n", max_subsequence_sum(a, 11));
+  printf("%i\n", max_subsequence_sum2(a, 11));
+  printf("%i\n", max_subsequence_sum4(a, 11));
+  printf("%i\n", max_subsequence_sum3(a, 0, 10));
   return 0;
 }
