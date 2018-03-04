5a522d60900a7b725459e7dd54b32a8404e1238e
youthy <yuyouqi101025@gmail.com>
2018-03-05 03:07:59 +0800

    udpate

--- datastructures/array.cc
+++ datastructures/array.cc
@@ -24,10 +24,31 @@ void left_rotote(int *arr, int n, int len)
     }
 }
 
+int ordered_2d_array_member(int *arr, int m, int n, int num)
+{
+    int cols, rows;
+    cols = rows = 0;
+    int tmp;
+    while(cols < m && *(arr + cols* m) <= num)
+    {
+        for(rows = 0; rows < n; rows++)
+        {
+            tmp = *(arr + cols * m + rows);
+            if(tmp == num)
+                return 1;
+            else if(tmp > num)
+                break;
+        }
+    }
+    return 0;
+}
+
 int main(void)
 {
     int a[10] = {1,2,3,4,5,6,7,8,9,10};
     left_rotote(a, 13, 10);
     print_array(a, 10);
+    int arr[4][4]={{1,2,3,4}, {3,5,7,9}, {5,6,7,8}, {7,9,10,11}};
+    printf("order 2d member:%d", ordered_2d_array_member(arr, 4, 4, 3));
     return 0;
 }
